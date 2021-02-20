882b6509b5ee28c41b87abf3ed6f57079e5e6d53
MyRepo21 <78832519+MyRepo21@users.noreply.github.com>
2021-02-20 17:12:19 -0500

    Update clock.js

--- clock.js
+++ clock.js
@@ -1,1 +1,22 @@
+function getDateTime() {
+    var today = new Date();
+    var seconds = today.getSeconds();
+    var minutes = today.getMinutes();
+    var hours = today.getHours();
+    var dd = today.getDate();
+    var day = today.getDay();
+    var mm = today.getMonth();
+    var yyyy = today.getFullYear();
+    var time = (hours + ":" + minutes + ":" + seconds);
 
+const monthsOfYr = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']; 
+
+const daysOfWk = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+console.log(time + " " + " " + daysOfWk[day] + ", " + monthsOfYr[mm] + " " + dd + " " + yyyy);
+  
+//console.log(time);
+//console.log(daysOfWk[day] + ", " + monthsOfYr[mm] + " " + dd + " " + yyyy);
+}
+
+setInterval(getDateTime, 1000);
